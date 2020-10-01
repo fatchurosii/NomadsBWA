f1f568702c16e6e05a9e30908404930cfd618871
fatchurrosii <watashifrdesu@yahoo.com>
2020-10-01 23:40:39 +0700

    add count dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Transaction;
+use App\TravelPackage;
 
 class DashboardController extends Controller
 {
     public function index(Request $request){
-        return view ('pages.admin.dashboard');
+        return view ('pages.admin.dashboard',[
+            'travel_package' => TravelPackage::count(),
+            'transaction' => Transaction::count(),
+            'transaction_pending' => Transaction::with('transaction_status','PENDING')->count(),
+            'transaction_success' => Transaction::with('transaction_status','SUCCESS')->count()
+
+        ]);
     }
 }
